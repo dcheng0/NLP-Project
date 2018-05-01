21718a5710b2b6ef224f00be9328a48aa1fe1daf
ask788 <ahmadkhan2013@u.northwestern.edu>
2018-04-30 23:15:46 -0500

    Update cnn_layer_search.py

--- cnn_layer_search.py
+++ cnn_layer_search.py
@@ -1,3 +1,62 @@
+import keras
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, Conv1D, Flatten
+from keras.layers import Bidirectional, GlobalMaxPool1D, MaxPooling1D
+from keras.models import Model, Sequential
+from keras import initializers, regularizers, constraints, optimizers, layers
+
+submission = pd.read_csv('data/sample_submission.csv')
+
+def split_data_train_validate_test(data, train_percent=.7, validate_percent=.3, seed=None):
+    np.random.seed(seed)
+    shuffled_data = np.random.permutation(data)
+    
+    train = shuffled_data[0:int(train_percent*(data.shape[0])),:]
+    validate = shuffled_data[int(train_percent*(data.shape[0])):int((train_percent+validate_percent)*(data.shape[0])),:]
+    return train, validate
+
+def read_data():
+    train = pd.read_csv('data/train.csv')
+    test = pd.read_csv('data/test.csv')
+    list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+    y = train[list_classes].values
+    list_sentences_train = train["comment_text"]
+    list_sentences_test = test["comment_text"]
+    return list_sentences_train, list_sentences_test, y
+
+def tokenizeComments(list_sentences_train, list_sentences_test, n):
+    max_words = n
+    tokenizer = Tokenizer(num_words=max_words)
+    tokenizer.fit_on_texts(list(list_sentences_train))
+    list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
+    list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+    return list_tokenized_train, list_tokenized_test
+
+def getCommentLengthsDistribution(comments):
+    commentsList = []
+    for i in range(0, len(comments)):
+        commentsList.append(len(comments[i]))
+    
+    plt.hist(commentsList,bins = np.arange(0,500,10))
+    plt.show()
+
+def padComments(list_tokenized_train, list_tokenized_test):
+    maxlen = 250
+    X_train = pad_sequences(list_tokenized_train, maxlen=maxlen)
+    X_test = pad_sequences(list_tokenized_test, maxlen=maxlen)
+    return X_train, X_test
+
+
+def buildFeedForwardModel(X_train, y, input_len, e, batch, l, opt, valid_split):
+    # define the architecture for the simple feed forward network
+    model = Sequential()
+    model.add(Dense(1024, input_dim=input_len, kernel_initializer="uniform", activation="sigmoid"))
+    model.add(Dropout(0.25))
+    model.add(Dense(512, kernel_initializer="uniform", activation="sigmoid"))
     model.add(Dropout(0.25))
     model.add(Dense(6))
     model.add(Activation("softmax"))
@@ -99,6 +158,7 @@ cnn_model_3layer = buildCNNModel(x_train, y, max_features = MAX_FEATURES, filter
 y_pred_1 = predictModel(cnn_model_1layer, x_test)
 y_pred_2 = predictModel(cnn_model_2layer, x_test)
 y_pred_3 = predictModel(cnn_model_3layer, x_test)
+
 
 list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 submission[list_classes] = y_pred_1
